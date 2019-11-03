e21bb291806592d23618ed802d64581053d9db66
Mike <mikezhao980306@gmail.com>
2019-11-03 15:29:48 -0800

    Ability to register for iron bronco

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,26 +1,3 @@
-/* Each account will be created as an object with attributes of userName and passWord*/
-var users = [];
-
-const addUser = (ev)=>{
-    ev.preventDefault();
-    let user = {
-        id:Date.now(),
-        userName: document.getElementById('userName').value,
-        password: document.getElementById('password').value
-    }
-    users.push(user);
-    document.querySelector('form').reset();
-
-
-    //saving to local storage
-    localStorage.setItem('UserList', JSON.stringify(users));
-}
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('btn').addEventListener('click', addUser);
-});
-
-
-/*login page code
 const checkLoginInfo = (ev)=>{
     ev.preventDefault();
     document.querySelector('form').reset();
@@ -42,4 +19,3 @@ const checkLoginInfo = (ev)=>{
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('btn').addEventListener('click', checkLoginInfo);
 });
-*/
