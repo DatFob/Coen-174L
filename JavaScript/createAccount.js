b588e6fef4ee637fd42230ddf91a68d3df68951b
Mike <mikezhao980306@gmail.com>
2019-11-02 19:21:08 -0700

    Saving username & password into users array

--- JavaScript/createAccount.js
+++ JavaScript/createAccount.js
@@ -1,42 +1,20 @@
 /* Each account will be created as an object with attributes of userName and passWord*/
 var users = [];
-var first_password;
-var second_password;
-var name;
-var users_index = 0;
 
-var person = function(user, pass){
-    this.userName = user;
-    this.passWord = pass;
-}
-
-/* Check if password & confirmed password are the same*/
-function passwordCheck(a,b){
-    if(a!==b){
-        return false;
+const addUser = (ev)=>{
+    ev.preventDefault();
+    let user = {
+        id:Date.now(),
+        userName: document.getElementById('userName').value,
+        password: document.getElementById('password').value
     }
-    return true;
+    users.push(user);
+    document.querySelector('form').reset();
+
+
+    //saving to local storage
+    localStorage.setItem('UserList', JSON.stringify(users));
 }
-
-/*Copy values of username and password to local variables name, first & second password*/
-function getValues(){
-    name = document.getElementById("createForm").elements.namedItem("username");
-    first_password = document.getElementById("createForm").elements.namedItem("password");
-    second_password = document.getElementById("createForm").elements.namedItem("confirmPassword");
-}
-
-document.querySelectorAll('input[type="button"]').addEventListener('click', getValues);
-
-/*Check passwords then create an object, putting it into users array*/
-function createAccount(){
-    if(passwordCheck(first_password,second_password)==false){
-        console.log("Passwords dont match");
-        return;
-    }
-    var user = new person(name,first_password);
-    users[user_index] = user;
-    user_index++;
-}
-
-createAccount();
-
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn').addEventListener('click', addUser);
+});
