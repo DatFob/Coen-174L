195a8498e95e9b93e0fe5e30762578150cff131a
Joseph Sindelar <38262363+jcsindelar@users.noreply.github.com>
2019-11-14 11:44:37 -0800

    Add files via upload

--- front-end/index.js
+++ front-end/index.js
@@ -50,13 +50,13 @@ function clearInput(){
 
 function onSignIn(googleUser) 
 {
-    //var id_token = googleUser.getAuthResponse().id_token;
-    //verifyIdToken(id_token);
+    var id_token = googleUser.getAuthResponse().id_token;
+    verifyIdToken(id_token);
     var profile = googleUser.getBasicProfile();
-    //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    //console.log('Name: ' + profile.getName());
-    //console.log('Image URL: ' + profile.getImageUrl());
-    //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
 function verifyIdToken(id_token)
