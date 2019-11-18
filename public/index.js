5fa4fe1514dfc8e23828833451eb19754034188a
Emma Allegrucci <emmallegrucci@gmail.com>
2019-11-18 13:41:25 -0800

    working sign in

--- public/index.js
+++ public/index.js
@@ -47,28 +47,28 @@ var userEmail, userName;
 //     document.getElementById('inputEmail3').value = '';
 // }
 
-function onSignIn(googleUser) 
-{
-    var id_token = googleUser.getAuthResponse().id_token;
-    verifyIdToken(id_token);
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    userEmail = profile.getEmail();
-    userName = profile.getName();
-    handleClientLoad();
-    // document.location.href="https://test-login-1573079166139.firebaseapp.com/IronBroncoHome.html";
-    storeUser();
-    var myUserEntity = {};
-    myUserEntity.id = profile.getId();
-    myUserEntity.name = profile.getName();
+// function onSignIn(googleUser) 
+// {
+//     var id_token = googleUser.getAuthResponse().id_token;
+//     verifyIdToken(id_token);
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+//     userEmail = profile.getEmail();
+//     userName = profile.getName();
+//     handleClientLoad();
+//     // document.location.href="https://test-login-1573079166139.firebaseapp.com/IronBroncoHome.html";
+//     // storeUser();
+//     var myUserEntity = {};
+//     myUserEntity.id = profile.getId();
+//     myUserEntity.name = profile.getName();
 
-    sessionStorage.setItem('myUserEntity',JSON.stringify(myUserEntity));
+//     sessionStorage.setItem('myUserEntity',JSON.stringify(myUserEntity));
 
-    alert(profile.getName());
-}
+//     alert(profile.getName());
+// }
 
 function checkIfLoggedIn(){
   if(sessionStorage.getItem('myUserEntity') == null) {
@@ -134,35 +134,35 @@ function verifyIdToken(id_token)
 //   });
 // });
 
-function storeUser(){
-    const firebaseConfig = {
-        apiKey: "AIzaSyCCcz2sIMLOFhT6Ltj9DSjvDdoFaPNehd0",
-        authDomain: "test-login-1573079166139.firebaseapp.com",
-        databaseURL: "https://test-login-1573079166139.firebaseio.com",
-        projectId: "test-login-1573079166139",
-        storageBucket: "test-login-1573079166139.appspot.com",
-        messagingSenderId: "1042080648547",
-        appId: "1:1042080648547:web:42a92c14b913d229909756",
-        measurementId: "G-WQ9Z1673RK"
-    };
-    firebase.initializeApp(firebaseConfig);
-    var firestore = firebase.firestore();
+// function storeUser(){
+//     // const firebaseConfig = {
+//     //     apiKey: "AIzaSyCCcz2sIMLOFhT6Ltj9DSjvDdoFaPNehd0",
+//     //     authDomain: "test-login-1573079166139.firebaseapp.com",
+//     //     databaseURL: "https://test-login-1573079166139.firebaseio.com",
+//     //     projectId: "test-login-1573079166139",
+//     //     storageBucket: "test-login-1573079166139.appspot.com",
+//     //     messagingSenderId: "1042080648547",
+//     //     appId: "1:1042080648547:web:42a92c14b913d229909756",
+//     //     measurementId: "G-WQ9Z1673RK"
+//     // };
+//     // firebase.initializeApp(firebaseConfig);
+// //     var firestore = firebase.firestore();
 
-    const docRef = firestore.collection("users");
+//     const docRef = firestore.collection("users");
 
-    docRef.add({
-        bike: 0,
-        run: 0,
-        swim: 0,
-        total: 0,
-        email: userEmail,
-        name: userName
-    })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
-}
+//     docRef.add({
+//         bike: 0,
+//         run: 0,
+//         swim: 0,
+//         total: 0,
+//         email: userEmail,
+//         name: userName
+//     })
+//     .then(function(docRef) {
+//         console.log("Document written with ID: ", docRef.id);
+//     })
+//     .catch(function(error) {
+//         console.error("Error adding document: ", error);
+//     });
+// }
 
