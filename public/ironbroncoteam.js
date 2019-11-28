642e67fed4a66fedbc0d122f2b7e4b983f40ca61
Emma Allegrucci <emmallegrucci@gmail.com>
2019-11-28 07:18:27 -0800

    team page logout works

--- public/ironbroncoteam.js
+++ public/ironbroncoteam.js
@@ -1,10 +1,10 @@
 var userEmail = JSON.parse(localStorage.getItem('email'));
 var userName = JSON.parse(localStorage.getItem('userName'));
 var teamName, teamCount;
-var memTeamName, memberRun, memberBike, memberSwim, memberTotal;
-var teamMember1, mem1Biking, mem1Running, mem1Swimming;
-var teamMember2, mem2Biking, mem2Running, mem2Swimming;
-var teamMember3, mem3Biking, mem3Running, mem3Swimming;
+var memTeamName, memberName, memberRun, memberBike, memberSwim, memberTotal;
+var teamMember1, mem1Name, mem1Biking, mem1Running, mem1Swimming;
+var teamMember2, mem2Name, mem2Biking, mem2Running, mem2Swimming;
+var teamMember3, mem3Name, mem3Biking, mem3Running, mem3Swimming;
 
 const firebaseConfig = {
     apiKey: "AIzaSyCCcz2sIMLOFhT6Ltj9DSjvDdoFaPNehd0",
@@ -62,6 +62,7 @@ function memberData(member){
             console.log("User data received");
             memTeamName = doc.data().team;
             if(teamName == memTeamName){
+                memberName = doc.data().name;
                 memberRun = doc.data().run;
                 memberBike = doc.data().bike;
                 memberSwim = doc.data().swim;
@@ -84,9 +85,11 @@ function displayTeam(){
 
     //get team member 1 data
     memberData(teamMember1);
+    mem1Name = memberName;
     mem1Biking = memberBike;
     mem1Running = memberRun;
     mem1Swimming = memberSwim;
+    document.getElementById("user1Name").innerHTML = mem1Name;
     document.getElementById("user1Swim").innerHTML = mem1Swimming + "/2.4";
     document.getElementById("user1Run").innerHTML = mem1Running + "/26.2";
     document.getElementById("user1Bike").innerHTML = mem1Biking + "/112";
@@ -94,29 +97,33 @@ function displayTeam(){
     //get team member 2 data and display
     if(teamCount >= 2){
         memberData(teamMember2);
+        mem2Name = memberName;
         mem2Biking = memberBike;
         mem2Running = memberRun;
         mem2Swimming = memberSwim;
+        document.getElementById("user2Name").innerHTML = mem2Name;
         document.getElementById("user2Swim").innerHTML = mem2Swimming + "/2.4";
         document.getElementById("user2Run").innerHTML = mem2Running + "/26.2";
         document.getElementById("user2Bike").innerHTML = mem2Biking + "/112";
     }
     else{
-        document.getElementById("nomember2").innerHTML = "No Member 2";
+        //document.getElementById("nomember2").innerHTML = "No Member 2";
     }
 
     //get team member 3 data and display
     if(teamCount >= 3){
         memberData(teamMember3);
+        mem3Name = memberName;
         mem3Biking = memberBike;
         mem3Running = memberRun;
         mem3Swimming = memberSwim;
+        document.getElementById("user3Name").innerHTML = mem3Name;
         document.getElementById("user3Swim").innerHTML = mem3Swimming + "/2.4";
         document.getElementById("user3Run").innerHTML = mem3Running + "/26.2";
         document.getElementById("user3Bike").innerHTML = mem3Biking + "/112";
     }   
     else{
-        document.getElementById("nomember3").innerHTML = "No Member 3";
+        //document.getElementById("nomember3").innerHTML = "No Member 3";
     }
 }
 
