6afd00d34125f274e60598dbf734b8da7c1f8215
Emma Allegrucci <emmallegrucci@gmail.com>
2019-12-01 07:42:35 -0800

    remove user works for iron bronco admin

--- public/ironbroncomanageusers.js
+++ public/ironbroncomanageusers.js
@@ -3,6 +3,7 @@ var userName = JSON.parse(localStorage.getItem('userName'));
 var users = [];
 var topUsers = [];
 var uTeam, uSwim, uBike, uRun, uTotal;
+var teamM1, teamM2, teamM3, teamSwimming, teamRunning, teamBiking, teamTotal, teamCount;
 
 const firebaseConfig = {
     apiKey: "AIzaSyCCcz2sIMLOFhT6Ltj9DSjvDdoFaPNehd0",
@@ -26,14 +27,14 @@ function displayUsers(){
 }
 
 function displayEachUser(item, index) {
-    document.getElementById("alphabeticalUsers").innerHTML += users[index].name + "<br>"; 
+    document.getElementById("alphabeticalUsers").innerHTML += users[index] + "<br>"; 
 }
 
 function userData(){
     userDocRef.orderBy('name').get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
             console.log(doc.id, " => ", doc.data());
-            users.push({name:doc.id});
+            users.push(doc.id);
         });
     });
 }
@@ -56,21 +57,50 @@ function uData(user){
     });
 }
 
+function teamInfo(team) {
+    teamDocRef.doc(team).get().then(function(doc) {
+        if (doc.exists) {
+            console.log("User data received");
+            teamM1 = doc.data().member1;
+            teamM2 = doc.data().member2;
+            teamM3 = doc.data().member3;
+            teamSwimming = doc.data().swim;
+            teamRunning = doc.data().run;
+            teamBiking = doc.data().bike;
+            teamTotal = doc.data().total;
+            teamCount = doc.data().memberCnt;
+        } else {
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+}
+
 //Finish removeUser()
 //need to remove from team then delete from database
 function removeUser()
 {
-    user = document.getElementById('user').value;
-    if (userExists == true) {
+    var user = document.getElementById('user').value;
+    console.log(user);
+    console.log(users);
+    if (userExists(user, users) == true) {
         uData(user);
         userDocRef.doc(user).delete().then(function() {
             console.log("User successfully deleted!");
         }).catch(function(error) {
             console.error("Error removing user: ", error);
         });
-        if(uTeam != null && uTeam != '')
-            teamDocRef.doc(uTeam).update({
-                member1: '',  
+        if(uTeam != null && uTeam != '') {
+            teamInfo(uTeam);
+            if (teamCount == 1) {
+                teamDocRef.doc(uTeam).delete().then(function() {
+                    console.log("Team successfully deleted!");
+                }).catch(function(error) {
+                    console.error("Error removing team: ", error);
+                });
+            }
+            teamDocRef.doc(uTeam).update({ 
                 swim: teamSwimming - uSwim,
                 run: teamRunning - uRun,
                 bike: teamBiking - uBike,
@@ -81,6 +111,34 @@ function removeUser()
             }).catch(function(error){
                 console.log('error occured');
             });
+            if (user == teamM1) {
+                teamDocRef.doc(uTeam).update({ 
+                    member1: ''
+                }).then(function(){
+                    console.log('success'); 
+                }).catch(function(error){
+                    console.log('error occured');
+                });
+            }
+            else if (user == teamM2) {
+                teamDocRef.doc(uTeam).update({ 
+                    member2: ''
+                }).then(function(){
+                    console.log('success'); 
+                }).catch(function(error){
+                    console.log('error occured');
+                });
+            }
+            else if (user == teamM3) {
+                teamDocRef.doc(uTeam).update({ 
+                    member3: ''
+                }).then(function(){
+                    console.log('success'); 
+                }).catch(function(error){
+                    console.log('error occured');
+                });
+            }
+        }
         alert(user + " was removed. Please refresh the page to view this change.");
     }
     else {
@@ -88,9 +146,10 @@ function removeUser()
     }
 }
 
-function userExists(user)
+function userExists(user, users)
 {
     for(var i = 0; i < users.length; i++) {
+        console.log(users[i]);
         if(users[i] == user){
             return true;
         } 
